108867cd0a070d3987f49f4838ca7631233a6cfd
Anthony Posselli <anthony.posselli@gmail.com>
2019-08-22 00:01:07 -0600

    Begin creating the game.
    Create pickup classes for the player (they are functional but may need tweaking as we begin implementing). Create a player class and add various members and methods needed to by the pickups (should be using to get the player working with these methods). Add placeholder visual assets. Add a game state object and a fail condition.

---
+++ NunchuckGame/GameState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NunchuckGame
+{
+    class GameState
+    {
+        private int Score;
+        private SpriteFont font;
+        private List<Pickup> ActivePickups;
+        private bool IsGameOver;
+
+        public GameState()
+        {
+            Score = 0;
+            ActivePickups = new List<Pickup>();
+            IsGameOver = false;
+        }
+
+        public void AddActivePickup(Pickup pickup)
+        {
+            Score += pickup.GetScoreChange();
+            if (pickup.GetDuration() > 0)
+                ActivePickups.Add(pickup);
+            else if (pickup.GetEndsGame())
+                IsGameOver = true;
+        }
+
+        public void Update(GameTime gameTime, ref Player player)
+        {
+            double deltaTime = gameTime.ElapsedGameTime.TotalSeconds;
+            player.SetSpeed(1f);
+
+            int count = 0;
+            while (count < ActivePickups.Count)
+            {
+                // Remove pickups whose effects should no longer be active
+                ActivePickups[count].UpdateDuration(gameTime.ElapsedGameTime.TotalSeconds);
+                if (ActivePickups[count].GetRemainingDuration() <= 0)
+                {
+                    ActivePickups.RemoveAt(count);
+                    continue;
+                }
+
+                // Update the player from the effects of the pickup
+                if (ActivePickups[count].GetAffectsPlayer())
+                    ActivePickups[count].Update(ref player);
+
+                count++;
+            }
+        }
+
+        public void SetFont(SpriteFont spriteFont)
+        {
+            font = spriteFont;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            string score = "Score: " + Score.ToString();
+            spriteBatch.DrawString(font, score, new Vector2(10, 10), Color.Black);
+
+            if (IsGameOver)
+            {
+                spriteBatch.DrawString(font, "Game Over", new Vector2(10, 40), Color.Red);
+            }
+        }
+    }
+}
---
+++ NunchuckGame/NunchuckGame.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace NunchuckGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class NunchuckGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+        GameState gameState;
+
+        public NunchuckGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            player = new Player();
+            gameState = new GameState();
+            //gameState.AddActivePickup(new SpeedPickup());
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D texture = Content.Load<Texture2D>("player");
+            float scale = 0.3f;
+
+            // Load the player resources
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X + 
+                GraphicsDevice.Viewport.TitleSafeArea.Width / 2 - texture.Width * scale / 2, 
+                GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2 -
+                texture.Height * scale / 2);
+            player.Initialize(texture, playerPosition, scale);
+
+            // Set the game font
+            gameState.SetFont(Content.Load<SpriteFont>("font"));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Handling input should come first. Player.Rotation and Player.SetDirection() should be used.
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Game state is updated second and may override or ignore user input
+            gameState.Update(gameTime, ref player);
+
+            // The player gets moved
+            player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+
+            gameState.Draw(spriteBatch);
+
+            // Stop drawing
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
