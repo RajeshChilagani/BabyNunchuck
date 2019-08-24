2dcce8748d04b7193862055d96da1e8f178c1aa9
Azhar Siddiqui <u1271730@ad.utah.edu>
2019-08-24 14:58:21 -0600

    Azhar - collision detection and response

---
+++ Code/Game1/Game1/Sprites/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1.Sprites
+{
+    public class Player : Sprite
+    {
+        public Player(Texture2D texture) : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            Move();
+
+            foreach (var sprite in sprites)
+            {
+                if (sprite == this)
+                    continue;
+
+                if ((this.Velocity.X > 0 && this.IsTouchingLeft(sprite)) ||
+                    (this.Velocity.X < 0 && this.IsTouchingRight(sprite)))
+                {
+                    this.Velocity.X = 0;
+                }
+
+
+                if ((this.Velocity.Y > 0 && this.IsTouchingTop(sprite)) ||
+                    (this.Velocity.Y < 0 && this.IsTouchingBottom(sprite)))
+                    this.Velocity.Y = 0;
+            }
+
+            Position += Velocity;
+
+            Velocity = Vector2.Zero;
+        }
+
+        private void Move()
+        {
+            if (Keyboard.GetState().IsKeyDown(input.Left))
+            {
+                Velocity.X = -Speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(input.Right))
+            {
+                Velocity.X = Speed;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(input.Up))
+            {
+                Velocity.Y = -Speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(input.Down))
+            {
+                Velocity.Y = Speed;
+            }
+        }
+    }
+}
